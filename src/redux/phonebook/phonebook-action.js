9e31525692b7be5e5cc2da65391deb4a4de402a4
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-06 19:54:16 +0300

    Create phonebook-action.js

---
+++ src/redux/phonebook/phonebook-action.js
@@ -0,0 +1,16 @@
+import { createAction } from '@reduxjs/toolkit';
+
+const addContact = createAction('ADD_CONTACT', ({ name, number }) => {
+  return {
+    payload: {
+      name: name,
+      number: number,
+    },
+  };
+});
+
+const deleteContact = createAction('DELETE_CONTACT');
+
+const changeFilter = createAction('CHANGE_FILTER');
+
+export default { addContact, deleteContact, changeFilter };
