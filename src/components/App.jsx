ce9052db85cb9aa4150bdbfa563171a8425ff3a1
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-06 19:53:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
+import React from 'react';
+import Form from './Form/Form';
+import Filter from './Filter/Filter';
+import NameList from './NameList/NameList';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      <Form />
+
+      <h2>Contacts</h2>
+      <Filter />
+      <NameList />
+    </>
   );
-};
+}
