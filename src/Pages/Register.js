3b5f6f34ecc6d72b29ede4eb607c1a7447fdd76c
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-08 23:14:03 +0300

    Update Register.js

---
+++ src/Pages/Register.js
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { useAddUserMutation } from '../redux/auth/auth-operations';
+import styles from './styles.module.css';
+
+export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [addUser] = useAddUserMutation();
+
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    addUser({ name, email, password });
+
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Register Form</h1>
+
+        <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+          <label style={styles.label}>
+            Имя
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label style={styles.label}>
+            Почта
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label style={styles.label}>
+            Пароль
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button type="submit">Зарегистрироваться</button>
+        </form>
+      </div>
+    </>
+  );
+}
