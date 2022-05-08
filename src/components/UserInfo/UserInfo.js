72538622ce73de272932773b11f2d76489c982ef
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-08 23:13:42 +0300

    Create UserInfo.js

---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,11 @@
+export default function UserMenu() {
+
+    return (
+      <div>
+        <span>Добро пожаловать, user</span>
+        {/* <button type="button" onClick={() => dispatch(authOperations.logOut())}>
+          Выйти
+        </button> */}
+      </div>
+    );
+  }
