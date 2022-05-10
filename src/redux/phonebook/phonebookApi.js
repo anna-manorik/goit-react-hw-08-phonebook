ee5fc5d7e72a5cedf7bdc3f36165f2817c4af7a5
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-10 22:04:47 +0300

    Update phonebookApi.js

--- src/redux/phonebook/phonebookApi.js
+++ src/redux/phonebook/phonebookApi.js
@@ -4,16 +4,20 @@ export const contactsApi = createApi({
   reducerPath: 'contactsApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://connections-api.herokuapp.com',
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   tagTypes: ['Contacts'],
+  refetchOnMountOrArgChange: true,
   endpoints: builder => ({
     getContacts: builder.query({
-      query: () => {
-          return {
-            url: `/contacts`,
-            method: 'GET',
-          }
-      },
+      query: () => `/contacts`,
       providesTags: ['Contacts'],
     }),
     addContact: builder.mutation({
@@ -30,15 +34,19 @@ export const contactsApi = createApi({
       invalidatesTags: ['Contacts'],
     }),
     deleteContact: builder.mutation({
-        query: id => {
-          return {
-            url: `/contacts/${id}`,
-            method: 'DELETE',
-          }
-        },
-        invalidatesTags: ['Contacts'],
-      }),
+      query: id => {
+        return {
+          url: `/contacts/${id}`,
+          method: 'DELETE',
+        };
+      },
+      invalidatesTags: ['Contacts'],
+    }),
   }),
 });
 
-export const { useGetContactsQuery, useAddContactMutation, useDeleteContactMutation } = contactsApi;
+export const {
+  useGetContactsQuery,
+  useAddContactMutation,
+  useDeleteContactMutation,
+} = contactsApi;
