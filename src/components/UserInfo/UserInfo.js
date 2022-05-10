1e0f86380cb7405345ea2b6ed32f5acd22a76e41
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-10 14:18:05 +0300

    Update UserInfo.js

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,11 +1,17 @@
+import { useSelector } from 'react-redux';
+import authSelectors from '../../redux/auth/auth-selectors';
+import { useLogoutMutation } from '../../redux/auth/authApi';
+
 export default function UserMenu() {
+  const userName = useSelector(authSelectors.getUserName);
+  const [logout] = useLogoutMutation();
 
     return (
       <div>
-        <span>Добро пожаловать, user</span>
-        {/* <button type="button" onClick={() => dispatch(authOperations.logOut())}>
-          Выйти
-        </button> */}
+        <span>Welcome, {userName}</span>
+        <button type="button" onClick={() => logout()}>
+          LogOut
+        </button>
       </div>
     );
   }
