dc0be6a9b74a8d59e71e3eb073516ffd25ee108e
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-10 22:04:51 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,19 +9,20 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
-// import { setupListeners } from '@reduxjs/toolkit/query';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import storage from 'redux-persist/lib/storage';
 import { usersApi } from './auth/authApi';
 import authReducer from './auth/auth-slice';
-import {contactsApi} from './phonebook/phonebookApi';
-import contactsReducer from './phonebook/phonebook-reducer';
-
+import { contactsApi } from './phonebook/phonebookApi';
+// import contactsReducer from './phonebook/phonebook-reducer';
+import { filterReducer } from './phonebook/phonebook-reducer';
 
 const reducers = combineReducers({
   [usersApi.reducerPath]: usersApi.reducer,
   auth: authReducer,
   [contactsApi.reducerPath]: contactsApi.reducer,
-  // contacts: contactsReducer,
+  // contacts: contactsReducer;
+  filterReducer: filterReducer,
 });
 
 const authPersistConfig = {
@@ -35,15 +36,15 @@ const persistedReducer = persistReducer(authPersistConfig, reducers);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
+  middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }).concat(
-      usersApi.middleware,
-    ),
+    }).concat(usersApi.middleware).concat(contactsApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export const persistor = persistStore(store);
 
