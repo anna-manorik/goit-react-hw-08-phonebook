d91c2d582233bb0835716352708d1aa32a7d608f
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-10 22:04:42 +0300

    Update phonebook-selectors.js

--- src/redux/phonebook/phonebook-selectors.js
+++ src/redux/phonebook/phonebook-selectors.js
@@ -1,13 +1,8 @@
 
-const getContacts = state => state.rootReduser.contactReduser;
-const getFilter = state => state.rootReduser.filterReduser;
-
-export default function getVisibleContacts(state) {
-  const contacts = getContacts(state);
-  const filter = getFilter(state);
-
+export function getVisibleContacts(contacts, filterValue) {
+  if(contacts){
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+      contact.name.toLowerCase().includes(filterValue.toLowerCase())
     );
+  }
 }
-
