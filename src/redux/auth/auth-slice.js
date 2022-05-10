0ab5f7664de3cc0c9516b84f82690b044ad22b40
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-10 14:18:20 +0300

    Update auth-slice.js

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { usersApi } from './authApi';
 
 const initialState = {
   user: { name: null, email: null },
@@ -9,7 +10,33 @@ const initialState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {},
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      usersApi.endpoints.addUser.matchFulfilled,
+      (state, { payload }) => {
+        state.token = payload.token;
+        state.user = payload.user;
+        state.isLoggedIn = true;
+      }
+    ),
+    builder.addMatcher(
+      usersApi.endpoints.login.matchFulfilled,
+      (state, { payload }) => {
+        state.token = payload.token;
+        state.user = payload.user;
+        state.isLoggedIn = true;
+      }
+    ),
+    builder.addMatcher(
+      usersApi.endpoints.logout.matchFulfilled,
+      (state, { payload }) => {
+        state.token = null;
+        state.user = { name: null, email: null };
+        state.isLoggedIn = false;
+      }
+    )
+  },
 });
 
 export default authSlice.reducer;
