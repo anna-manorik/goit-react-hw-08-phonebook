be843e9cb218ee5c935d894843f12f35a5aee1bb
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-10 14:18:09 +0300

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,61 @@
+import { useState } from 'react';
+import { useLoginMutation } from '../redux/auth/authApi';
+import styles from './styles.module.css';
+
 export default function Login() {
-    return (
-      <>
-        <div>Login</div>
-      </>
-    );
-  }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [login] = useLoginMutation();
+
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    switch (name) {
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    login({ email, password });
+
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h1>Страница логина</h1>
+
+      <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+        <label style={styles.label}>
+          Почта
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label style={styles.label}>
+          Пароль
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Войти</button>
+      </form>
+    </div>
+  );
+}
