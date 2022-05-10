dca4af8624bb8e05a41ab91d52c5071bd9839c83
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-10 22:04:32 +0300

    Update phonebook-reducer.js

--- src/redux/phonebook/phonebook-reducer.js
+++ src/redux/phonebook/phonebook-reducer.js
@@ -1,26 +1,27 @@
-import { createReducer, combineReducers } from '@reduxjs/toolkit';
+import { createReducer, createSlice } from '@reduxjs/toolkit';
 import actions from './phonebook-action';
-import shortid from 'shortid';
+// import { contactsApi } from './phonebookApi';
 
-const contactReduser = createReducer([], {
-  [actions.addContact]: (state, { payload }) => {
-    const contact = {
-      id: shortid.generate(),
-      name: payload.name,
-      number: payload.number,
-    };
-
-    return [contact, ...state];
-  },
-  [actions.deleteContact]: (state, { payload }) =>
-    state.filter(({ id }) => id !== payload),
-});
-
-const filterReduser = createReducer('', {
+export const filterReducer = createReducer('', {
   [actions.changeFilter]: (state, { payload }) => payload,
 });
 
-export default combineReducers({
-  contactReduser,
-  filterReduser,
-});
+// const contactsSlice = createSlice({
+//   name: 'contacts',
+//   initialState: [],
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder.addMatcher(
+//       contactsApi.endpoints.addContact.matchFulfilled,
+//       (state, { payload }) => {
+//         [...state, payload];
+//       },
+//     ),
+//     builder.addMatcher(
+//       contactsApi.endpoints.deleteContact.matchFulfilled,
+//       (state, { payload }) => state.filter(({ id }) => id !== payload),
+//     )
+//   },
+// });
+
+// export default contactsSlice.reducer;
