cfda9fcdb2e210b4368f8b4b26ee10e239674dd5
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-10 22:04:22 +0300

    Delete index.js

--- src/redux/phonebook/index.js
+++
@@ -1,4 +0,0 @@
-// export { default as phonebookOperations } from './phonebook-operations';
-export { default as phonebookSelectors } from './phonebook-selectors';
-export * from './phonebook-action';
-export { default as phonebookReducer } from './phonebook-reducer';
