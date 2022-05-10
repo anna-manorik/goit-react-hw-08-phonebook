eb0d854996bf9419b1e731e62f2088362ad0a3d1
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-10 14:17:57 +0300

    Update AppBar.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import UserInfo from './UserInfo/UserInfo';
+import authSelectors from '../redux/auth/auth-selectors';
 import styles from './appBar.module.css';
 
 export default function AppBar() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
   return (
     <div className={styles.container}>
       <div>
@@ -21,22 +25,26 @@ export default function AppBar() {
         </NavLink>
       </div>
 
-      <div>
-        <NavLink
-          to="/register"
-          className={({ isActive }) => (isActive ? styles.active : styles.link)}
-        >
-          Register
-        </NavLink>
-        <NavLink
-          to="/login"
-          className={({ isActive }) => (isActive ? styles.active : styles.link)}
-        >
-          Login
-        </NavLink>
-      </div>
-
-      <UserInfo />
+      {isLoggedIn ?
+        <UserInfo />
+       : (<div>
+          <NavLink
+            to="/register"
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.link
+            }
+          >
+            Register
+          </NavLink>
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.link
+            }
+          >
+            Login
+          </NavLink>
+        </div>)}
     </div>
   );
 }
