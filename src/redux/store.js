4936fbc2d5eb36d95e143b69ee98e90acfc056d5
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-11 17:18:18 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,28 +14,25 @@ import storage from 'redux-persist/lib/storage';
 import { usersApi } from './auth/authApi';
 import authReducer from './auth/auth-slice';
 import { contactsApi } from './phonebook/phonebookApi';
-// import contactsReducer from './phonebook/phonebook-reducer';
 import { filterReducer } from './phonebook/phonebook-reducer';
 
 const reducers = combineReducers({
   [usersApi.reducerPath]: usersApi.reducer,
   auth: authReducer,
   [contactsApi.reducerPath]: contactsApi.reducer,
-  // contacts: contactsReducer;
   filterReducer: filterReducer,
 });
 
 const authPersistConfig = {
-  key: 'root',
+  key: 'auth',
   storage,
-  timeout: null,
   whitelist: ['token'],
 };
 
 const persistedReducer = persistReducer(authPersistConfig, reducers);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {persistedReducer},
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
