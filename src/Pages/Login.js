74969d56abee94a774a3d9f6892bfecb8b74b285
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-15 19:45:52 +0300

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -42,6 +42,8 @@ export default function Login() {
             name="email"
             value={email}
             onChange={handleChange}
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            required
           />
         </label>
 
@@ -52,6 +54,8 @@ export default function Login() {
             name="password"
             value={password}
             onChange={handleChange}
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2, 3}$"
+            required
           />
         </label>
 
