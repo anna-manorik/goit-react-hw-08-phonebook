939060c387a7b87a5c01221c305266b999d90165
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-21 14:03:57 +0300

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,13 @@
-
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <div>Всем добрій дінь</div>
+      <div>Hello! This is app for creating and saving your phonebook contacts. 
+      <p>Please <Link to="/register">register</Link> or <Link to="/login">login</Link></p></div>
+
+      
     </>
   );
 }
